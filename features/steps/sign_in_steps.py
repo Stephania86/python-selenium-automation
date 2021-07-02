0a6cf5bcdf33442701c59244fd0caca15f6967e9
stephania <stephaniahomme@gmail.com>
2021-07-01 20:55:47 -0400

    Homework 6

--- features/steps/sign_in_steps.py
+++ features/steps/sign_in_steps.py
@@ -1,8 +1,18 @@
 from selenium.webdriver.common.by import By
 from behave import given, when, then
+from time import sleep
+
+@given('Open Amazon Product {product_id} page')
+def  open_amazon_product(context, product_id):
+   context.driver.get(f'https://www.amazon.com/dp/{product_id}')
 
 
-@then("verify sign page oppened")
-def verify_sign_page_oppened(context):
-    # context.driver.find_element(By.ID, 'app_email')
-    assert 'https://www.amazon.com/ap/signin' in context.driver.current_url
+@then('Verify user can click through colors')
+def verify_loop_can_thru_colors(context):
+   expected_colors = ['Black', 'Blue', 'Burgundy', 'Caramel', 'Gray', 'Green', 'Khaki', 'Pink', 'White', 'Yellow']
+   color_webelements = context.driver.find_elements(By.CSS_SELECTOR, "#variation_color_name li")
+
+   for i in range(len(color_webelements)):
+      color_webelements[i].click()
+      actual_text = context.driver.find_element(By.CSS_SELECTOR, "#variation_color_name span.selection").text
+      assert actual_text == expected_colors[i], f'Error, color is {actual_text}, but expected{expected_colors[i]}'
--- features/tests/sign-in.feature
+++ features/tests/sign-in.feature
@@ -1,7 +1,17 @@
 Feature: test for sign in page
 
-  Scenario logged out when clicking sign in page
-    Given Open Amazon page
-    When Click orders
-    Then verify sign page oppened
+    Scenario logged out user sees Sign in page when clicking Orders
+      Given Open Amazon page
+      When Click orders Then verify sign page opened
+
+
+    Scenario: Sign in page can be opened from Sing In popup
+      Given Open Amazon page
+      When Click Sign In from popup
+      Then Verify Sign In page opened
+
+
+    Scenario: User can select all colors
+      Given Open Amazon Product B081YS2F7N page
+      Then Verify user can click through colors
 
